dcdf8d281540962fa53fdf8743c8a809653a5140
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-12-03 16:14:38 +0100

    quick sort strings

--- Algorithms-DataStruct/quickSortStrings.js
+++ Algorithms-DataStruct/quickSortStrings.js
@@ -1,25 +1,25 @@
-function QuickSort(array) {
+const names = require("./names.js");
+
+function QuickSortStrings(array) {
   if (array.length <= 1) {
     return array;
   }
 
-  let sorted = [];
   let pivot = array[0];
   let smaller = [];
   let bigger = [];
 
   for (let i = 1; i < array.length; i++) {
-    if (pivot >= array[i]) {
+    if (array[i].localeCompare(pivot) <= 0) {
       smaller.push(array[i]);
     } else {
       bigger.push(array[i]);
     }
   }
 
-  return [...QuickSort(smaller), pivot, ...QuickSort(bigger)];
+  return [...QuickSortStrings(smaller), pivot, ...QuickSortStrings(bigger)];
 }
 
-const unsorted = [3, 5, 1, 4, 3, 2, 6, 10, 5, 6, 11, 8];
-console.log("Unsorted: ", unsorted);
-const sorted = QuickSort(unsorted);
+console.log("Unsorted Names: ", names);
+const sorted = QuickSortStrings(names);
 console.log("Sorted: ", sorted);
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # **CS50 Python Practice Files** by Felipe Moreira
+
 This repository contains Python files created while following Harvard's CS50 course. They cover basic Python concepts like loops, conditionals, file handling, and arrays for hands-on practice.
+
+# **Algorithms and Data Structures Practice Files** by Felipe Moreira
+
+This repository contains JavaScript files created while following freeCodeCapm.com's Algorithms and Data Structures Full Course. They cover basic JS concepts like Arrays, Recursion, Binary Search, Merge Sort, Quick Sort, for hands-on practice.
